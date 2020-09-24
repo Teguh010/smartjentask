2f823a64afd02bfd754a481b78a2e76ca5db9119
teguh baghoy <teguhbaghoy6@gmail.com>
2020-09-24 16:56:18 +0700

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-page-container class="container">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style >
+.container {
+height: 100vh;
+
+}
+
+</style>
